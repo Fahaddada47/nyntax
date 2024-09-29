07e5fec637055ce9c9baf0ee4f86ec46dae7dc41
Fahad Islam <fifahad007@gmail.com>
2024-09-29 17:24:51 +0600

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:nyntax/application/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:nyntax/firebase_options.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await GetStorage.init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
 
